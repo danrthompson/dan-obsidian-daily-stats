6dcedcf0556b6e5ec42e408a42311aa5b00ef511
Dan Thompson <danielrthompsonjr@gmail.com>
2023-02-13 18:06:47 -0500

    small changes to update it as my own plugin

---
+++ setup_plugin.sh
@@ -0,0 +1,4 @@
+npm run build
+cp main.js "/Users/danthompson/Obsidian/Synced Vault/.obsidian/plugins/dan-daily-stats/main.js"
+cp manifest.json "/Users/danthompson/Obsidian/Synced Vault/.obsidian/plugins/dan-daily-stats/manifest.json"
+cp styles.css "/Users/danthompson/Obsidian/Synced Vault/.obsidian/plugins/dan-daily-stats/styles.css"
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,32 @@
-import { TFile, Plugin, MarkdownView, debounce, Debouncer, WorkspaceLeaf, addIcon } from 'obsidian';
-import { VIEW_TYPE_STATS_TRACKER } from './constants';
-import StatsTrackerView from './view';
+import {
+	TFile,
+	Plugin,
+	MarkdownView,
+	debounce,
+	Debouncer,
+	WorkspaceLeaf,
+	addIcon,
+} from "obsidian";
+import { VIEW_TYPE_STATS_TRACKER } from "./constants";
+import StatsTrackerView from "./view";
 
 interface WordCount {
 	initial: number;
 	current: number;
 }
 
-interface DailyStatsSettings {
+interface DDailyStatsSettings {
 	dayCounts: Record<string, number>;
-	todaysWordCount: Record<string, WordCount>;
+	dayToWordCount: Record<string, Record<string, WordCount>>;
 }
 
-const DEFAULT_SETTINGS: DailyStatsSettings = {
+const DEFAULT_SETTINGS: DDailyStatsSettings = {
 	dayCounts: {},
-	todaysWordCount: {}
-}
+	dayToWordCount: {},
+};
 
-export default class DailyStats extends Plugin {
-	settings: DailyStatsSettings;
+export default class DDailyStats extends Plugin {
+	settings: DDailyStatsSettings;
 	statusBarEl: HTMLElement;
 	currentWordCount: number;
 	today: string;
@@ -37,22 +45,32 @@ export default class DailyStats extends Plugin {
 			this.currentWordCount = 0;
 		}
 
-		this.debouncedUpdate = debounce((contents: string, filepath: string) => {
-			this.updateWordCount(contents, filepath);
-		}, 400, false);
+		this.debouncedUpdate = debounce(
+			(contents: string, filepath: string) => {
+				this.updateWordCount(contents, filepath);
+			},
+			400,
+			false
+		);
 
 		this.registerView(
 			VIEW_TYPE_STATS_TRACKER,
-			(leaf: WorkspaceLeaf) => (this.view = new StatsTrackerView(leaf, this.settings.dayCounts))
+			(leaf: WorkspaceLeaf) =>
+				(this.view = new StatsTrackerView(
+					leaf,
+					this.settings.dayCounts
+				))
 		);
 
 		this.addCommand({
-			id: "show-daily-stats-tracker-view",
-			name: "Open tracker view",
+			id: "dan-show-daily-stats-tracker-view",
+			name: "Dan Open tracker view",
 			checkCallback: (checking: boolean) => {
 				if (checking) {
 					return (
-						this.app.workspace.getLeavesOfType(VIEW_TYPE_STATS_TRACKER).length === 0
+						this.app.workspace.getLeavesOfType(
+							VIEW_TYPE_STATS_TRACKER
+						).length === 0
 					);
 				}
 				this.initLeaf();
@@ -60,20 +78,30 @@ export default class DailyStats extends Plugin {
 		});
 
 		this.registerEvent(
-			this.app.workspace.on("quick-preview", this.onQuickPreview.bind(this))
+			this.app.workspace.on(
+				"quick-preview",
+				this.onQuickPreview.bind(this)
+			)
 		);
 
 		this.registerInterval(
 			window.setInterval(() => {
-				this.statusBarEl.setText(this.currentWordCount + " words today ");
+				this.statusBarEl.setText(
+					this.currentWordCount + " words today "
+				);
 			}, 200)
 		);
 
-		addIcon("bar-graph", `<path fill="currentColor" stroke="currentColor" d="M122.88,105.98H9.59v-0.02c-2.65,0-5.05-1.08-6.78-2.81c-1.72-1.72-2.79-4.11-2.79-6.75H0V0h12.26v93.73h110.62V105.98 L122.88,105.98z M83.37,45.6h19.55c1.04,0,1.89,0.85,1.89,1.89v38.46c0,1.04-0.85,1.89-1.89,1.89H83.37 c-1.04,0-1.89-0.85-1.89-1.89V47.5C81.48,46.46,82.33,45.6,83.37,45.6L83.37,45.6z M25.36,22.07h19.55c1.04,0,1.89,0.85,1.89,1.89 v62c0,1.04-0.85,1.89-1.89,1.89H25.36c-1.04,0-1.89-0.85-1.89-1.89v-62C23.47,22.92,24.32,22.07,25.36,22.07L25.36,22.07 L25.36,22.07z M54.37,8.83h19.54c1.04,0,1.89,0.85,1.89,1.89v75.24c0,1.04-0.85,1.89-1.89,1.89H54.37c-1.04,0-1.89-0.85-1.89-1.89 V10.72C52.48,9.68,53.33,8.83,54.37,8.83L54.37,8.83z"/>`);
-		this.registerInterval(window.setInterval(() => {
-			this.updateDate();
-			this.saveSettings();
-		}, 1000));
+		addIcon(
+			"bar-graph",
+			`<path fill="currentColor" stroke="currentColor" d="M122.88,105.98H9.59v-0.02c-2.65,0-5.05-1.08-6.78-2.81c-1.72-1.72-2.79-4.11-2.79-6.75H0V0h12.26v93.73h110.62V105.98 L122.88,105.98z M83.37,45.6h19.55c1.04,0,1.89,0.85,1.89,1.89v38.46c0,1.04-0.85,1.89-1.89,1.89H83.37 c-1.04,0-1.89-0.85-1.89-1.89V47.5C81.48,46.46,82.33,45.6,83.37,45.6L83.37,45.6z M25.36,22.07h19.55c1.04,0,1.89,0.85,1.89,1.89 v62c0,1.04-0.85,1.89-1.89,1.89H25.36c-1.04,0-1.89-0.85-1.89-1.89v-62C23.47,22.92,24.32,22.07,25.36,22.07L25.36,22.07 L25.36,22.07z M54.37,8.83h19.54c1.04,0,1.89,0.85,1.89,1.89v75.24c0,1.04-0.85,1.89-1.89,1.89H54.37c-1.04,0-1.89-0.85-1.89-1.89 V10.72C52.48,9.68,53.33,8.83,54.37,8.83L54.37,8.83z"/>`
+		);
+		this.registerInterval(
+			window.setInterval(() => {
+				this.updateDate();
+				this.saveSettings();
+			}, 1000)
+		);
 
 		if (this.app.workspace.layoutReady) {
 			this.initLeaf();
@@ -85,7 +113,9 @@ export default class DailyStats extends Plugin {
 	}
 
 	initLeaf(): void {
-		if (this.app.workspace.getLeavesOfType(VIEW_TYPE_STATS_TRACKER).length) {
+		if (
+			this.app.workspace.getLeavesOfType(VIEW_TYPE_STATS_TRACKER).length
+		) {
 			return;
 		}
 		this.app.workspace.getRightLeaf(false).setViewState({
@@ -122,16 +152,27 @@ export default class DailyStats extends Plugin {
 
 	updateWordCount(contents: string, filepath: string) {
 		const curr = this.getWordCount(contents);
-		if (this.settings.dayCounts.hasOwnProperty(this.today)) {
-			if (this.settings.todaysWordCount.hasOwnProperty(filepath)) {//updating existing file
-				this.settings.todaysWordCount[filepath].current = curr;
-			} else {//created new file during session
-				this.settings.todaysWordCount[filepath] = { initial: curr, current: curr };
-			}
-		} else {//new day, flush the cache
-			this.settings.todaysWordCount = {};
-			this.settings.todaysWordCount[filepath] = { initial: curr, current: curr };
+		let todayWordCount = this.settings.dayToWordCount[this.today];
+		if (!todayWordCount) {
+			this.settings.dayToWordCount[this.today] = {};
+			todayWordCount = this.settings.dayToWordCount[this.today];
 		}
+		if (todayWordCount.hasOwnProperty(filepath)) {
+			//updating existing file
+			todayWordCount[filepath].current = curr;
+		} else {
+			// TODO: i believe this will be triggered whether it is actually a new file
+			// or if the file was just opened for the first time in the session
+			// i need to differentiate between the two, and if it is new, set the
+			// initial value to 0
+
+			//created new file during session
+			todayWordCount[filepath] = {
+				initial: curr,
+				current: curr,
+			};
+		}
+
 		this.updateCounts();
 	}
 
@@ -141,16 +182,31 @@ export default class DailyStats extends Plugin {
 	}
 
 	updateCounts() {
-		this.currentWordCount = Object.values(this.settings.todaysWordCount).map((wordCount) => Math.max(0, wordCount.current - wordCount.initial)).reduce((a, b) => a + b, 0);
+		let todayWordCount = this.settings.dayToWordCount[this.today];
+		if (!todayWordCount) {
+			this.settings.dayToWordCount[this.today] = {};
+		}
+		this.currentWordCount = Object.values(
+			this.settings.dayToWordCount[this.today]
+		)
+			.map((wordCount) =>
+				Math.max(0, wordCount.current - wordCount.initial)
+			)
+			.reduce((a, b) => a + b, 0);
 		this.settings.dayCounts[this.today] = this.currentWordCount;
 	}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
-		if (Object.keys(this.settings.dayCounts).length > 0) { //ensuring we never reset the data by accident
+		if (Object.keys(this.settings.dayCounts).length > 0) {
+			//ensuring we never reset the data by accident
 			await this.saveData(this.settings);
 		}
 	}
